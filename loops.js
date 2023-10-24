196a677d13ce42e4f64994a9a2001e83f6b169f2
sahomelabs <sahomelabs@gmail.com>
2023-10-24 14:20:42 -0400

    complete bank_account labD

--- loops.js
+++ loops.js
@@ -30,17 +30,33 @@
 // C. Give me Five
 // For the numbers 0 - 100, print out "I found a number. High five!" if the number is a multiple of five
 // Example Output:   
-for (let i = 1; i<100; i++) {
-    if (i % 5 == 0 && i % 3 == 0) {
-        console.log("I found a "+ i + ". High five! Three is a crowd");
-        } else {
-            if (i % 5 == 0) {
-                console.log("I found a "+ i +" High five!")
-            } else {
-                if (i %3 == 0) {
-                    console.log("I found a "+ i +". Three is crowd")
-                }
-            }
-        }
-}
+// for (let i = 1; i<100; i++) {
+//     if (i % 5 == 0 && i % 3 == 0) {
+//         console.log("I found a "+ i + ". High five! Three is a crowd");
+//         } else {
+//             if (i % 5 == 0) {
+//                 console.log("I found a "+ i +" High five!")
+//             } else {
+//                 if (i %3 == 0) {
+//                     console.log("I found a "+ i +". Three is crowd")
+//                 }
+//             }
+//         }
+// }
 
+// D. Savings account
+// Write code that will save the sum of all the numbers between 1 - 10 to a variable called bank_account.
+// Check your work! Your bank_account should have $55 in it.
+let bank_account = 0
+    for (let i=1; i<=10; i++){
+    bank_account += i
+        }
+        console.log("Your bank account has $" + bank_account);
+        
+// You got a bonus! Your pay is now doubled each week. Write code that will save the sum of all the numbers between 1 - 100 multiplied by 2.
+// Check your work! Your bank_account should have $10,100 in it.
+        bank_account = 0
+            for (let i=1; i<=100; i++){
+                bank_account += i *2
+                }
+                console.log("Your bank account has $" + bank_account);       
