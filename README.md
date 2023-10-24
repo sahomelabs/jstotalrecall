cd54de35e8b923a4e6c6bbf6660435969b96b3e1
sahomelabs <sahomelabs@gmail.com>
2023-10-24 11:08:46 -0400

    Complete Variables&Datatypes

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
 // Variables & Datatypes
-To answer these questions, you can add them in a multiline comment section inside of script.js
-
-like this:
---
+++ jsreviewlab.js
@@ -0,0 +1,30 @@
+/* ==================== */ 
+// Variables & Datatypes // 
+
+// Question 1 : How do we assign a value to variable 
+let questionOne = 5;
+
+// Question2 : How do we assign a value to a variable? 
+Question2 = 30;
+console.log(Question2); // Output: 30
+
+// Question 3: How do we assign an existing variable to a new variable?
+let new_variable_question3 = 40;
+console.log(new_variable_question3);
+
+// Question 4 : Remind me, what are declare, assign, and define?
+// Answers: 
+// Declaring a variable we can use let, or const 
+// Assigning a value to a variable is assigning the value using the assignment operator (=)
+// Defining a function is creating a block of code that performs a specific task when called
+
+// Question5 : What is pseudocoding and why should you do it?
+// Answer: Pseudocoding is writing out in plain English how a computer program would work. It helps us visualize our thought process as well as
+
+// Question6 : What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+// Answer: Problem solving and coding depend on the complexity of the the problem. 
+// I will say no estimate of how long it will take. Experience matter also an experice programmer can solve issue faster compare to beginner 
+
+
+
+
